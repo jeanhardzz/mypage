ba0328e2c054f7b4393247f51dfa99531a59fff6
Fulano de Tal <jean.lk@hotmail.com>
2022-08-31 12:07:38 -0300

    v0.2

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
+#IMPORTACOES
+
 import streamlit as st
 import time 
 
+#CONFIGURACOES BASICAS DA PAGINA
 st.set_page_config(
     page_title="Jean Mota",
     page_icon="🍌",
     layout="centered",    
 )
 
-st.title("Opa Tudo Bom!")
+#CSS
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html= True)
 
 with st.sidebar:
 
-    html = "<h1>Opa tudo bem? 👋 Eu sou o Jean</h1><h3>Skills</h3>"
-
+    html = "<h1>Menu vazio ainda</h1>"
     st.markdown(html,unsafe_allow_html=True)
-    img_python = "<img align='center' alt='Python' height='30' width='40' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'>"
-    st.markdown(img_python, unsafe_allow_html=True)
+    
 
-    img_c_plus="<img align='center' alt='C++' height='30' width='40' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg'>"
-    st.markdown(img_c_plus, unsafe_allow_html=True)
-
-    img_c="<img align='center' alt='C' height='30' width='40' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg'>"
-    st.markdown(img_c, unsafe_allow_html=True)
-
+with st.container():        
+    st.title("Curriculum Vitae")
+    """    
+    * * *
+    """
 
 with st.container():
-    """
-    What is Lorem Ipsum?
+    
+    st.title(":scroll:Jean Lucas Almeida Mota")
+    """        
+    :inbox_tray: Download da versão em pdf aqui
 
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    :calendar: 12/06/1997
+
+    :phone: (31) 99468-8276
+    """
+
+    link = "<div align='left' style='display: inline_block'><a href='https://www.linkedin.com/in/jean-lucas-almeida-mota-957b0b152/'><img align='center' src='https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white' alt=''></a>  <a href='https://codeforces.com/profile/jeanhardzz'><img align='center' src='https://img.shields.io/badge/Codeforces-445f9d?style=for-the-badge&logo=Codeforces&logoColor=whiteg' alt=''/></a>  <a href='mailto:jean.lk@hotmail.com'><img align='center' src='https://img.shields.io/badge/Microsoft_Outlook-0078D4?style=for-the-badge&logo=microsoft-outlook&logoColor=white' alt=''/></a>  <a href='https://www.instagram.com/jean.lk/'><img align='center' src='https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white' alt=''/></a>  <a href='https://twitter.com/Jean_lucks'><img align='center' src='https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white' alt=''/></a>"
+    st.markdown(link, unsafe_allow_html=True)
+    """
     * * *
     """
 
