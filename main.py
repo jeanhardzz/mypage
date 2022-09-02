ba4605fec49ec42cb78d0ba633e093d97ac2c96d
Fulano de Tal <jean.lk@hotmail.com>
2022-09-02 12:20:19 -0300

    v0.4.1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import time 
 from PIL import Image
+from streamlit_option_menu import option_menu
 
 #CONFIGURACOES BASICAS DA PAGINA
 st.set_page_config(
@@ -14,6 +15,34 @@ st.set_page_config(
 #CSS
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html= True)
+
+def navigation():
+    try:
+        path = st.experimental_get_query_params()['p'][0]
+    except Exception as e:
+        st.error('Please use the main app.')
+        return None
+    return path
+
+
+#NAVBAR
+#usar no futuro "border": "solid 1px rgba(160, 160, 160, 0.3)","         
+        
+        
+select = option_menu(
+    menu_title="",
+    options=["CV","APPS","ESTUDOS"],
+    default_index=0,    
+    orientation="horizontal",
+    styles={
+        "container": {"border": "solid 1px rgba(160, 160, 160, 0.3)","padding": "5!important", "background-color": "#f4f4f4"},
+        "icon": {"font-size": "0px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "center", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {},
+                
+    }
+)
+
 
 #MENU LATERAL
 with st.sidebar:
