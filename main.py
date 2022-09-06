55e60f38f62f4894d6df29a553efd769464a34a8
Fulano de Tal <jean.lk@hotmail.com>
2022-09-06 11:47:39 -0300

    v0.5.0

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 import time 
 from PIL import Image
 from streamlit_option_menu import option_menu
+from views import cv
 
 #CONFIGURACOES BASICAS DA PAGINA
 st.set_page_config(
@@ -31,7 +32,7 @@ def navigation():
         
 select = option_menu(
     menu_title="",
-    options=["CV","APPS","ESTUDOS"],
+    options=["CV","APPS","EXTRAS"],
     default_index=0,    
     orientation="horizontal",
     styles={
@@ -63,57 +64,11 @@ with st.sidebar:
     #html = "<h1>Meu Perfil</h1>"
     #st.markdown(html,unsafe_allow_html=True)
     
+if(select == "CV"):
+    cv.cv()
 
-#CURRICULUM VITAE
-with st.container():        
-    st.title("Curriculum Vitae")
-    """    
-    * * *
-    """
+elif(select == "APPS"):
+    st.title("APPS")
 
-with st.container():        
-    """
-    ## ![pdf](https://img.icons8.com/ios/30/000000/pdf--v1.png) Jean Lucas Almeida Mota
-
-    ![Download](https://img.icons8.com/material-outlined/20/000000/downloading-updates.png) Download da versão em pdf [aqui](https://drive.google.com/file/d/15h2VtB7J8FaxceLiHX4x3pFUCY3K1zth/view?usp=sharing)
-    * ![Data Nascimento](https://img.icons8.com/ios-filled/18/000000/birthday.png) 12/06/1997
-    * ![Whatsapp](https://img.icons8.com/material-outlined/20/000000/whatsapp--v1.png) (31) 99468-8276
-    * ![Linkedin](https://img.icons8.com/ios-glyphs/20/000000/linkedin-2--v1.png) [linkedin.com/in/jeanlkmota](https://www.linkedin.com/in/jeanlkmota/)
-    * ![Github](https://img.icons8.com/ios-glyphs/20/000000/github.png) [github.com/jeanhardzz](https://github.com/jeanhardzz)                   
-    * ![Email](https://img.icons8.com/ios-filled/20/000000/gmail.png) [jeanlkmota@gmail.com](mailto:jeanlkmota@gmail.com)               
-    * ![Instagram](https://img.icons8.com/ios-glyphs/20/000000/instagram-new.png) [instagram.com/jean.lk/](https://www.instagram.com/jean.lk/)               
-    * ![Codeforces](https://img.icons8.com/external-tal-revivo-bold-tal-revivo/20/000000/external-codeforces-programming-competitions-and-contests-programming-community-logo-bold-tal-revivo.png) [codeforces.com/profile/jeanhardzz](https://codeforces.com/profile/jeanhardzz)              
-    * ![Twitter](https://img.icons8.com/ios-glyphs/20/000000/twitter--v1.png) [twitter.com/Jean_lucks](https://twitter.com/Jean_lucks)              
-    
-    * * *
-
-    ## ![pdf](https://img.icons8.com/ios/25/000000/info--v1.png) Sobre mim
-
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
-
-    * * *
-
-    ## ![pdf](https://img.icons8.com/ios/25/000000/rocket--v1.png) Habilidades
-
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
-    
-    * * *
-
-    ## ![pdf](https://img.icons8.com/ios/25/000000/project-management.png) Projetos/Planos
-
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
-    
-    * * *
-
-    ## ![pdf](https://img.icons8.com/glyph-neue/30/000000/university.png) Formação Acadêmica    
-
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
-    
-    * * *
-
-    ## ![pdf](https://img.icons8.com/ios/25/000000/new-job.png) Profissional
-
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
-    
-    * * *
-    """
+elif(select == "EXTRAS"):
+    st.title("EXTRAS")    
---
+++ views/cv.py
@@ -0,0 +1,95 @@
+import streamlit as st
+
+def cv():
+    #CURRICULUM VITAE
+    with st.container():        
+        st.title("Curriculum Vitae")
+        curriculo = """    
+        * * *
+        
+        ## ![pdf](https://img.icons8.com/ios/30/000000/pdf--v1.png) Jean Lucas Almeida Mota
+
+        ![Download](https://img.icons8.com/material-outlined/20/000000/downloading-updates.png) Download da versão em pdf [aqui](https://drive.google.com/file/d/15h2VtB7J8FaxceLiHX4x3pFUCY3K1zth/view?usp=sharing)
+        * ![Data Nascimento](https://img.icons8.com/ios-filled/18/000000/birthday.png) 12/06/1997
+        * ![Whatsapp](https://img.icons8.com/material-outlined/20/000000/whatsapp--v1.png) (31) 99468-8276
+        * ![Linkedin](https://img.icons8.com/ios-glyphs/20/000000/linkedin-2--v1.png) [linkedin.com/in/jeanlkmota](https://www.linkedin.com/in/jeanlkmota/)
+        * ![Github](https://img.icons8.com/ios-glyphs/20/000000/github.png) [github.com/jeanhardzz](https://github.com/jeanhardzz)                   
+        * ![Email](https://img.icons8.com/ios-filled/20/000000/gmail.png) [jeanlkmota@gmail.com](mailto:jeanlkmota@gmail.com)               
+        * ![Instagram](https://img.icons8.com/ios-glyphs/20/000000/instagram-new.png) [instagram.com/jean.lk/](https://www.instagram.com/jean.lk/)               
+        * ![Codeforces](https://img.icons8.com/external-tal-revivo-bold-tal-revivo/20/000000/external-codeforces-programming-competitions-and-contests-programming-community-logo-bold-tal-revivo.png) [codeforces.com/profile/jeanhardzz](https://codeforces.com/profile/jeanhardzz)              
+        * ![Twitter](https://img.icons8.com/ios-glyphs/20/000000/twitter--v1.png) [twitter.com/Jean_lucks](https://twitter.com/Jean_lucks)              
+        
+        * * *
+
+        ## ![pdf](https://img.icons8.com/ios/25/000000/info--v1.png) Sobre mim
+
+        Sou apaixonado por problemas difíceis e nada me motiva mais do que resolver problemas do mundo real com tecnologia e trabalho em equipe. Graduando em Matemática Computacional pela UFMG. Técnico em tecnologia da informação pelo Sistema FIEMG. Possuo fortes conhecimentos em matemática e em programação com foco em C,C++ e Python. E tenho muito interesse em trabalhar com empresas que usam seu know-how em projetos de software e iniciativas envolvendo Machine Learning, Blockchain e Data Science para solução de problemas.
+
+        Recentemente tenho me focado em Machine Learning. Acredito que existem inúmeras oportunidades para usar algoritmos e tecnologia para otimizar negócios e criar valor.
+
+        * * *
+
+        ## ![pdf](https://img.icons8.com/ios/25/000000/rocket--v1.png) Habilidades 
+          
+
+        <div class="skills">
+            <p class="alignleft">python</p>
+            <p class="alignright">
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot grey"></span> 
+            </p>
+            <div style="clear: both;"></div>
+            <p class="alignleft">C</p>
+            <p class="alignright">
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+            </p>
+            <div style="clear: both;"></div>
+            <p class="alignleft">C++</p>
+            <p class="alignright">
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+            </p>
+            <div style="clear: both;"></div>
+            <p class="alignleft">Matemática</p>
+            <p class="alignright">
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot black"></span>
+                <span class="dot grey"></span>
+            </p>
+            <div style="clear: both;"></div>
+        </div>
+        <div style="clear: both;"></div>   
+
+        * * *        
+
+        ## ![pdf](https://img.icons8.com/ios/25/000000/project-management.png) Projetos/Planos
+
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
+        
+        * * *
+
+        ## ![pdf](https://img.icons8.com/glyph-neue/30/000000/university.png) Formação Acadêmica    
+
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
+        
+        * * *
+
+        ## ![pdf](https://img.icons8.com/ios/25/000000/new-job.png) Profissional
+
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
+        
+        * * *
+        """
+        st.markdown(curriculo,unsafe_allow_html= True)        
