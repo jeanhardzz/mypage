ff9c5f44c613461c7ab4a3d33e76ea93edb6ac96
Jean Lucas <jean.lk@hotmail.com>
2022-09-06 14:34:46 -0300

    v0.5.6

--- views/cv.py
+++ views/cv.py
@@ -23,9 +23,9 @@ def cv():
 
         ## ![pdf](https://img.icons8.com/ios/25/000000/info--v1.png) Sobre mim
 
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sou apaixonado por problemas difíceis e nada me motiva mais do que resolver problemas do mundo real com tecnologia e trabalho em equipe. Graduando em Matemática Computacional pela UFMG. Técnico em tecnologia da informação pelo Sistema FIEMG. Possuo fortes conhecimentos em matemática e em programação com foco em C,C++ e Python. E tenho muito interesse em trabalhar com empresas que usam seu know-how em projetos de software e iniciativas envolvendo Machine Learning, Blockchain e Data Science para solução de problemas.
+        Sou apaixonado por problemas difíceis e nada me motiva mais do que resolver problemas do mundo real com tecnologia e trabalho em equipe. Graduando em Matemática Computacional pela UFMG. Técnico em tecnologia da informação pelo Sistema FIEMG. Possuo fortes conhecimentos em matemática e em programação com foco em C,C++ e Python. E tenho muito interesse em trabalhar com empresas que usam seu know-how em projetos de software e iniciativas envolvendo Machine Learning, Blockchain e Data Science para solução de problemas.
 
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Recentemente tenho me focado em Machine Learning. Acredito que existem inúmeras oportunidades para usar algoritmos e tecnologia para otimizar negócios e criar valor.
+        Recentemente tenho me focado em Machine Learning. Acredito que existem inúmeras oportunidades para usar algoritmos e tecnologia para otimizar negócios e criar valor.
 
         * * *
 
@@ -103,17 +103,18 @@ def cv():
 
         ## ![pdf](https://img.icons8.com/ios/25/000000/project-management.png) Projetos/Planos
         
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Graduando em Matemática Computacional pela UFMG e fazendo Iniciação Científica em Problemas Difíceis em Grafos com Vinicius Fernandes do Santos do Departamento da Computação (DCC). Tenho me dedicado de forma ativa em projetos pessoais, a fim de colocar em prática todo o meu conhecimento teórico. E faço cursos online, como também leio, constantemente, tanto sobre minha área de atuação, quanto sobre assuntos diversos como psicologia, oratória, filosofia, empreendedorismo e inovação. Meu lema é Stay Hungry Stay Foolish, inspirado pelo Steve Jobs.
+        Graduando em Matemática Computacional pela UFMG e fazendo Iniciação Científica em Problemas Difíceis em Grafos com Vinicius Fernandes do Santos do Departamento da Computação (DCC). Tenho me dedicado de forma ativa em projetos pessoais, a fim de colocar em prática todo o meu conhecimento teórico. E faço cursos online, como também leio, constantemente, tanto sobre minha área de atuação, quanto sobre assuntos diversos como psicologia, oratória, filosofia, empreendedorismo e inovação. Meu lema é Stay Hungry Stay Foolish, inspirado pelo Steve Jobs.
 
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Minha intenção com esta página é poder visualizar minha evolução, manter um portfólio dos meus projetos e quem sabe interagir com pessoas que se interessam por inovações tecnológicas e científicas.
+        Minha intenção com esta página é poder visualizar minha evolução, manter um portfólio dos meus projetos e quem sabe interagir com pessoas que se interessam por inovações tecnológicas e científicas.
 
         
         * * *
 
         ## ![pdf](https://img.icons8.com/glyph-neue/30/000000/university.png) Formação Acadêmica    
 
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical.
-        
+        - Curso de graduação em Matemática Computacional - conclusão em 2023/2 - UFMG - Universidade Federal de Minas Gerais
+        - Técnico em tecnologia da informação - concluído em 2014 - Sistema FIEMG - Federação das Indústrias do Estado de Minas Gerais
+      
         * * *
 
         ## ![pdf](https://img.icons8.com/ios/25/000000/new-job.png) Profissional
